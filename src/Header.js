f5f70e11c1a9ef07b86a47a3143b6f5200b325f6
DioenDJS <negociospacheti@gmail.com>
2021-12-11 16:50:25 -0300

    :art: import component searchInput

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,23 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { ClienteContext } from "./ClienteContext";
+import SearchInput from './SearchInput';
+import Conecta from "./Conecta";
 
 import "./Header.css";
 
 const Header = () => {
 
+  const [text, setText ] = useState('');
   const cliente = useContext(ClienteContext);
+
+  useEffect(() =>{
+    if(text){
+        const lista = Conecta.get(`camisetas/pesq/:${text}`)
+        console.log(lista.data);
+    }
+  }, [text]);
 
   let history = useHistory();
 
@@ -18,7 +28,7 @@ const Header = () => {
 
   return (
     <nav className="navbar navbar-expand-sm  navbar-dark bg-style">
-      <Link className="navbar-brand" to="/">
+      <Link className="navbar-brand  w-25" to="/">
         <img
           src="logo.png"
           alt="logo loja"
@@ -28,7 +38,9 @@ const Header = () => {
         <h3>geek t-shirts shop</h3>
         <h5>Camisetas em Destaque</h5>
       </Link>
-
+      <div className="m-auto  w-50 ">
+        <SearchInput value={text} onChange={(search) => setText(search)} />
+      </ div>
       <ul className="navbar-nav ml-auto">
         {
           (cliente.dados.id) && <li>
